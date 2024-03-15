2f88402acbd5a4927db09e38bdc88efc6326841f
Mohammadhusain <mohammadhusain.varaliya@bytestechnolab.com>
2024-03-15 11:39:55 +0530

    added service value in transport

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import * as nodemailer from 'nodemailer';
 
 interface ICreateTransport {
-    host: string,
-    port: number,
+    service: string,
     auth: {
         user: string,
         password: string
@@ -16,8 +15,7 @@ interface ICreateTransport {
 export async function sendEmail(body: ICreateTransport) {
     try {
         const transporter = nodemailer.createTransport({
-            host: body.host,
-            port: body.port,
+            service: body.service,
             auth: body.auth
         });
 
